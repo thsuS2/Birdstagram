7cb2ef9d03f112e9df88ce7ecbccbce21b47f97b
Jisoo-P <totojisoo@gmail.com>
2021-05-08 23:51:52 +0900

    First commit

---
+++ app/src/main/java/com/example/birdstagram_f16/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.birdstagram_f16
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
